6ddb748924fc2570d8616c725c3bb8a9ca6bec23
younghyun9 <apdlvmf556@naver.com>
2021-03-25 18:06:55 +0900

    배열 스위치

---
+++ src/용현/basic/day05/CardCheck.java
@@ -0,0 +1,53 @@
+package 용현.basic.day05;
+
+import java.util.Scanner;
+
+public class CardCheck {
+
+	public static void main(String[] args) {
+		// 신용카드 번호의 첫글자를 보고 어떤 종류와 은행인지 알려주는 프로그램
+		// 3: JCB카드
+		// 4: Visa카드
+		// 5: Master카드 
+		
+		String cardnum;
+		String cardtype = "";
+		String cardbank= "";
+		String fmt = "카드종류는 %s이고요, %s입니다";
+		Scanner sc = new Scanner(System.in);
+		String result;
+		
+		System.out.println("카드번호를 입력하세요 ");
+		cardnum = sc.next();
+		
+		//if (cardnum.charAt(0) == '3')
+			//cardtype = "JCB카드";
+		//else if (cardnum.charAt(0) == '4')
+			//cardtype = "Visa";
+		//else if (cardnum.charAt(0) == '5')
+		//	cardtype = "Master";                  //if else문 보다 switch문이 더 어울림ㅡ
+		
+		switch(cardnum.charAt(0)) {
+		case '3' : cardtype = "JCB 카드"; break;
+		case '4' : cardtype = "Visa 카드"; break;
+		case '5' : cardtype = "Master 카드"; break;
+		}
+		// 스위치문
+		//System.out.println(cardtype);
+		
+		switch(cardnum) {
+		case "356317" : cardbank = "NH농협카드"; break;
+		case "356901" : cardbank = "신한카드"; break;
+		case "356912" : cardbank = "KB국민카드"; break;
+		case "404825" : cardbank = "비씨카드"; break;
+		case "438676" : cardbank = "신한카드"; break;
+		case "457973" : cardbank = "국민카드"; break;
+		case "515594" : cardbank = "신한카드"; break;
+		case "524353" : cardbank = "외환카드"; break;
+		case "540926" : cardbank = "국민카드"; break;
+		} //스위치
+		
+		System.out.printf(fmt, cardtype, cardbank);
+	} // 메인
+
+}
---
+++ src/용현/basic/day05/LeapYear2.java
@@ -0,0 +1,32 @@
+package 용현.basic.day05;
+
+import java.util.Scanner;
+
+public class LeapYear2 {
+
+	public static void main(String[] args) {
+		// 윤년 여부를 출력하는 프로그램
+		// 지구의 공전주기와 자전주기의 불일치로 발생
+		// 공전주기 : 365.24일   
+		// 자전주기 : 0.24일
+		// 이 두간의 오차 때문에 4년에 하루를 추가하는 방법으로 오류를 보정함
+		// 조건1 : 4로나누어 떨어지지만, 조건2 : 100으로 나눠 떨어지지 않음 단, 400으로 나눠 떨어짐
+		
+		// 1600(윤년), 2020(윤년), 1900(평년) 2021(?)
+		
+		int year;
+		String result;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("년도를 입력하세요");
+		year = sc.nextInt();
+		
+		if (year % 4 == 0 && year % 100 != 0 || (year % 400 == 0))
+			result = "윤년입니다";
+		else 
+			result = "평년입니다";
+		
+		System.out.println(result);
+
+	}
+
+}
